c4b47571940cb659b3fdec05db02b64bdd714e72
Andrea <andr3z45@gmail.com>
2021-11-13 21:22:37 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -22,51 +22,74 @@ const news = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
 
-let itemsLibrary = '';
+const boxImmagini = document.querySelector('.box-immagini');
+const boxFull = document.querySelector('.box-fullimg');
+const boxNews = document.querySelector('.box-news');
 
 for (let i = 0; i < img.length; i++) {
-    itemsLibrary += `
+    // ciclo libreria
+    boxImmagini.innerHTML += `
     <div class="img-box">
         <img src=${img[i]} alt="${title[i]} anteprima">
-    </div > `
-    console.log(itemsLibrary);
+    </div> `
+
+
+    // ciclo fullimg immagini
+    boxFull.innerHTML += `
+    <div class="full-img-box">
+        <img src=${img[i]} alt="${title[i]} anteprima">
+    </div>
+    `
+
+    // ciclo news
+    boxNews.innerHTML += `
+    <div class="text-box">
+        <h2>${title[i]}</h2> 
+        <p>${news[i]}</p>
+    </div>
+    `
 }
 
-const boxImmagini = document.querySelector('.box-immagini');
-boxImmagini.innerHTML = itemsLibrary;
 
 const imgBox = document.getElementsByClassName('img-box');
-let activeImgbox = 0
+const fullImgBox = document.getElementsByClassName('full-img-box');
+const textBox = document.getElementsByClassName('text-box');
+let activeImgbox = 0;
 imgBox[activeImgbox].classList.add('active');
+fullImgBox[activeImgbox].classList.add('active');
+textBox[activeImgbox].classList.add('active');
 
 const up = document.querySelector('.up');
 const down = document.querySelector('.down');
 
 down.addEventListener("click", function () {
+    imgBox[activeImgbox].classList.remove('active');
+    fullImgBox[activeImgbox].classList.remove('active');
+    textBox[activeImgbox].classList.remove('active');
 
     if (activeImgbox < img.length - 1) {
-
-        imgBox[activeImgbox].classList.remove('active');
-
         activeImgbox++;
-
-        imgBox[activeImgbox].classList.add('active');
-
+    } else {
+        activeImgbox = 0;
     }
+
+    imgBox[activeImgbox].classList.add('active');
+    fullImgBox[activeImgbox].classList.add('active');
+    textBox[activeImgbox].classList.add('active');
 });
 
 up.addEventListener("click", function () {
+    imgBox[activeImgbox].classList.remove('active');
+    fullImgBox[activeImgbox].classList.remove('active');
+    textBox[activeImgbox].classList.remove('active');
 
     if (activeImgbox > 0) {
-
-        imgBox[activeImgbox].classList.remove('active');
-
         activeImgbox--;
-
-        imgBox[activeImgbox].classList.add('active');
-
+    } else {
+        activeImgbox = img.length - 1;
     }
 
-
-
-})
+    imgBox[activeImgbox].classList.add('active');
+    fullImgBox[activeImgbox].classList.add('active');
+    textBox[activeImgbox].classList.add('active');
+});
